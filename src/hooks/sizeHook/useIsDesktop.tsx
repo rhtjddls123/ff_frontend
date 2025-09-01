6c34892a69d96a8ed01dde55a2ccc054773cc0f6
unknown <jjjk0605@naver.com>
2025-09-01 23:12:54 +0900

    fix: 모바일, 데스크탑 화면 크기 알아오는 hook 항상 모바일을 반환하던것을 변경

--- src/hooks/sizeHook/useIsDesktop.tsx
+++ src/hooks/sizeHook/useIsDesktop.tsx
@@ -1,9 +1,8 @@
 import { useMediaQuery } from 'react-responsive';
-import { IS_MOBILE_IN_DESKTOP } from '@/constants';
 
 const useIsDesktop = () => {
   const isDesktop = useMediaQuery({ minWidth: 768 });
-  return !IS_MOBILE_IN_DESKTOP && isDesktop;
+  return isDesktop;
 };
 
 export default useIsDesktop;
